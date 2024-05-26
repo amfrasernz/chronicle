b2e747ae341bf08eec94c0ac9dd1df4c803082f3
Aimee Fraser <amfrasernz@gmail.com>
2024-05-27 09:17:45 +1200

    Project page changes

--- js/script.js
+++ js/script.js
@@ -1,32 +1,54 @@
-// $(document).ready(function() {
-//     $('#enterBtn').on('click', function() {
-//       $('#introScreen').addClass('fade-out');
-//       setTimeout(function() {
-//         $('#introScreen').addClass('d-none');
-//         $('#homeMainContent').removeClass('d-none');
-//       }, 1000);
-//       setTimeout(function() {
-//         $('#homeMainContent').addClass('fade-in');
-//       }, 1200);
-//     });
-//   });
+// // SHOWS INTRO SCREEN EACH TIME
+  // $(document).ready(function() {
+  //   $('#enterBtn').on('click', function() {
+  //     $('#introScreen').addClass('fade-out');
+  //     setTimeout(function() {
+  //       $('#introScreen').addClass('d-none');
+  //       $('#homeMainContent').removeClass('d-none');
+  //     }, 1000);
+  //     setTimeout(function() {
+  //       $('#homeMainContent').addClass('fade-in');
+  //     }, 1100);
+  //   });
+  // });
 
-
-  $(document).ready(function() {
-    $('#enterBtn').on('click', function() {
-      $('#introScreen').addClass('fade-out');
-      setTimeout(function() {
-        $('#introScreen').addClass('d-none');
-        $('#homeMainContent').removeClass('d-none');
-      }, 1000);
-      setTimeout(function() {
-        $('#homeMainContent').addClass('fade-in');
-      }, 1100);
-    });
-  });
-
-
-
+      // SHOWS INTRO SCREEN ONCE PER SESSION
+      $(document).ready(function() {
+        function getSessionItem(key) {
+          return sessionStorage.getItem(key);
+        }
+      
+        function setSessionItem(key, value) {
+          sessionStorage.setItem(key, value);
+        }
+      
+        function showIntroScreen() {
+          $('#introScreen').removeClass('d-none');
+          $('#homeMainContent').addClass('d-none');
+          $('#enterBtn').on('click', function() {
+            $('#introScreen').addClass('fade-out');
+            setTimeout(function() {
+              $('#introScreen').addClass('d-none');
+              $('#homeMainContent').removeClass('d-none');
+              setSessionItem('introShown', 'true');
+            }, 1000);
+            setTimeout(function() {
+              $('#homeMainContent').addClass('fade-in');
+            }, 1100);
+          });
+        }
+      
+        function showMainContent() {
+          $('#introScreen').addClass('d-none');
+          $('#homeMainContent').removeClass('d-none').addClass('fade-in');
+        }
+      
+        if (!getSessionItem('introShown')) {
+          showIntroScreen();
+        } else {
+          showMainContent();
+        }
+      });
 
 $(document).ready(function() {
     var $navbar = $('#navbar');
@@ -39,3 +61,18 @@ $(document).ready(function() {
       }
     });
   });
+
+
+  $(document).ready(function() {
+    $('.clamp').each(function() {
+        var lineHeight = parseInt($(this).css('line-height'));
+        var maxLines = 2;
+        var maxHeight = lineHeight * maxLines;
+        var actualHeight = $(this).height();
+        if (actualHeight > maxHeight) {
+            var text = $(this).text();
+            var truncatedText = text.slice(0, Math.floor(text.length * (maxHeight / actualHeight))) + '...';
+            $(this).text(truncatedText);
+        }
+    });
+});
