797251108ef10fbc4c6f087354f6b176ce815b15
Aimee Fraser <amfrasernz@gmail.com>
2024-06-03 10:34:31 +1200

    Working

--- js/script.js
+++ js/script.js
@@ -36,26 +36,26 @@ $(document).ready(function () {
     $('#introScreen').removeClass('d-none');
     $('#homeMainContent').addClass('d-none');
 
-      $('#enterBtn').on('click', function() {
+    $('#enterBtn').on('click', function () {
       $('#introScreen').addClass('fade-out');
-      setTimeout(function() {
+      setTimeout(function () {
         $('#introScreen').addClass('d-none');
         $('#homeMainContent').removeClass('d-none');
         setSessionItem('introShown', 'true');
       }, 1000);
-      setTimeout(function() {
+      setTimeout(function () {
         $('#homeMainContent').addClass('fade-in');
       }, 1100);
     });
 
     $(document).on('wheel', function () {
       $('#introScreen').addClass('fade-out');
-      setTimeout(function() {
+      setTimeout(function () {
         $('#introScreen').addClass('d-none');
         $('#homeMainContent').removeClass('d-none');
         setSessionItem('introShown', 'true');
       }, 1000);
-      setTimeout(function() {
+      setTimeout(function () {
         $('#homeMainContent').addClass('fade-in');
       }, 1100);
     });
@@ -72,50 +72,25 @@ $(document).ready(function () {
     showMainContent();
   }
 
-  // HOME PAGE SNAP TO SECTION
-  // const $sections = $('section');
-  // let currentIndex = 0;
 
-  // function scrollToSection(index) {
-  //   if (index >= 0 && index < $sections.length) {
-  //     $('html, body').animate({
-  //       scrollTop: $sections.eq(index).offset().top
-  //     }, 500);
-  //     currentIndex = index;
-  //   }
-  // }
-
-  // $(document).on('keydown', function(event) {
-  //   if (event.key === 'ArrowDown') {
-  //     scrollToSection(currentIndex + 1);
-  //   } else if (event.key === 'ArrowUp') {
-  //     scrollToSection(currentIndex - 1);
-  //   }
-  // });
-
-  // $('#homeMainContent').on('wheel', function(event) {
-  //   if (event.originalEvent.deltaY > 0) {
-  //     scrollToSection(currentIndex + 1);
-  //   } else if (event.originalEvent.deltaY < 0) {
-  //     scrollToSection(currentIndex - 1);
-  //   }
-  // });
   // PROJECTS
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const frames = document.querySelectorAll('.circle-frame');
-    frames.forEach(frame => {
-      const originalUrl = frame.style.backgroundImage;
-      const hoverUrl = frame.getAttribute('data-hover');
-      if (hoverUrl) {
-        frame.addEventListener('mouseover', () => {
-          frame.style.backgroundImage = `url(${hoverUrl})`;
-        });
-        frame.addEventListener('mouseout', () => {
-          frame.style.backgroundImage = originalUrl;
-        });
-      }
-    });
+  const $frames = $('.circle-frame');
+  $frames.each(function () {
+    const $frame = $(this);
+    const originalUrl = $frame.css('background-image');
+    const hoverUrl = $frame.data('hover');
+    if (hoverUrl) {
+      $frame.hover(
+        function () {
+          $frame.css('background-image', `url(${hoverUrl})`);
+        },
+        function () {
+          $frame.css('background-image', originalUrl);
+        }
+      );
+    }
   });
 });
+
+
 
